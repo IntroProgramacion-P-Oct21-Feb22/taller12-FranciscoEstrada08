478e05c430e46e17372c44e7a73a942a3af2e3fe
Francisco Estrada <fjestrada3@utpl.edu.ec>
2022-01-22 22:15:06 -0500

    primer commit

--- Ejercicios/src/ejercicios/Problema01.java
+++ Ejercicios/src/ejercicios/Problema01.java
@@ -15,7 +15,36 @@ public class Problema01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double[][] datos = {{10, 2}, {1, 5}, {6, 2}, {7, 8}};
+        
+        double[][] result = new double[4][2];
+        
+        result = factorial(datos);
+        for (int i = 0; i < datos.length; i++) {
+            for (int j = 0; j < datos[i].length; j++) {
+                System.out.printf("El factorial de %.0f es %.2f\t", datos[i][j],
+                        result[i][j]);
+            }
+            System.out.println();
+        }
+        
+    }
+    public static double[][] factorial(double [][] datos){
+        double factorial[][] = new double[4][2];
+        
+        for(int i = 0; i<datos.length;i++){
+            for(int j = 0; j< datos[i].length;j++){
+                factorial[i][j] = condicional(datos[i][j]);
+            }
+        }
+        return factorial;
+    }
+    public static double condicional(double numero){
+        if(numero<=1){
+            return 1;
+        }else{
+            return numero * condicional(numero-1);
+        }
     }
     
 }
--- Ejercicios/src/ejercicios/Problema04.java
+++ Ejercicios/src/ejercicios/Problema04.java
@@ -15,7 +15,33 @@ public class Problema04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        double[] bases = {100, 90, 80, 70, 60};
+        
+        double[] potencias = {2, 3, 2, 2, 1};
+        
+        double[] resultado = new double[5];
+        
+        for(int i = 0; i<bases.length;i++){
+            resultado[i] = obtenerPotencia(bases[i],potencias[i]);
+            System.out.printf("La potencia en la posicion %d es %.2f\n",i,resultado[i]);
+        }
+        System.out.printf("La suma total de las potencias es: "
+                + "%.0f\n", obtenerSuma(resultado, resultado.length));
     }
-    
+    public static double obtenerPotencia(double a,double b){
+        if(b==1){
+            return a;
+        }else{
+            return a * obtenerPotencia(a, b - 1);
+        }
+    }
+     public static double obtenerSuma(double[] resultado, int tamaño) {
+
+        if (tamaño == 1) {
+            return resultado[0];
+        } else {
+            return resultado[tamaño - 1] + obtenerSuma(resultado, tamaño - 1);
+        }
+    }
 }
